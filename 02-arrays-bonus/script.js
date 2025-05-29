37e1738e1dd38814fa4072b59e9c9f62be8c27bb
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-05-29 16:25:14 +0200

    Implement array reversal for teachers and log results

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -11,6 +11,10 @@ const teachers = [
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 const reversedTeachers = teachers.toReversed();
+/* for (let i = teachers.length; i >= 0; i--) {
+  reversedTeachers.push(teachers[i])
+  console.log(teachers[i])
+} */
 console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
