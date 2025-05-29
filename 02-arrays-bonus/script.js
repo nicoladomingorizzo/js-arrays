8a51154f9d18010d52fc508d1d3e4f4b5c17b08d
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-05-29 16:09:51 +0200

    Refactor array manipulations in JS fundamentals exercises 01 and 02

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,17 +10,34 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.toReversed();
+console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+for (let i = 0; i < teachers.length; i++) {
+  const item = teachers[i]
+  if (item.length >= 5) {
+    longNames.push(item)
+  }
+}
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
-
+const secondTeacher = teachers.indexOf('Ed');
+teachers.splice(secondTeacher, 1);
+console.log(teachers);
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
-
+let isFabioPresent = false;
+for (let i = 0; i < teachers.length; i++) {
+  console.log(teachers[i])
+  if (teachers[i] == 'Fabio') {
+    isFabioPresent = true
+  }
+}
+console.log(isFabioPresent);
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join();
+console.log(teachersString);
