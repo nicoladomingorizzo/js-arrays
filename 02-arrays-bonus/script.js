167fc16c6474171c1e3bc7193f0b5652f6da898d
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-05-29 16:32:56 +0200

    Refactor array reversal logic and clean up code in script.js

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,11 +10,10 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.toReversed();
-/* for (let i = teachers.length; i >= 0; i--) {
+const reversedTeachers = [];
+for (let i = teachers.length; i >= 0; i--) {
   reversedTeachers.push(teachers[i])
-  console.log(teachers[i])
-} */
+};
 console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
@@ -25,7 +24,7 @@ for (let i = 0; i < teachers.length; i++) {
   if (item.length >= 5) {
     longNames.push(item)
   }
-}
+};
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
@@ -40,7 +39,8 @@ for (let i = 0; i < teachers.length; i++) {
   if (teachers[i] == 'Fabio') {
     isFabioPresent = true
   }
-}
+};
+// const isFabioPresent = teachers.includes('Fabio');
 console.log(isFabioPresent);
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = teachers.join();
